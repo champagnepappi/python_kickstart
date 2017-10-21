a96bc8ed6ab1fe22930fde6c120b0c1c928e1e3e
kevin <kevinochieng548@gmail.com>
2017-10-21 18:00:25 +0300

    check if number of character appears more than once

--- isogram.py
+++ isogram.py
@@ -3,3 +3,11 @@ def is_isogram(word):
         return (word, False)
     elif type(word) != str:
         raise TypeError('Argument is a string')
+    else:
+        word = word.lower()
+        for char in word:
+            if word.count(char) > 1:
+                return (word, False)
+            else:
+                return (word, True)
+
