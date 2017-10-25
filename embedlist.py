6186495b083de23651585f15ffe95406336b5c01
kevin <kevinochieng548@gmail.com>
2017-10-25 13:10:46 +0300

    print list elements

--- embedlist.py
+++ embedlist.py
@@ -1,3 +1,5 @@
 movies = [ "Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
           ["Graham Chapman",
            ["Michael Palin", "John Cleese","Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+print movies
