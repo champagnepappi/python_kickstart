384eb5b46c237b23660326576ccd1aa52a1b5813
kevin <kevinochieng548@gmail.com>
2017-11-06 22:26:49 +0300

    use format characters

--- ex5.py
+++ ex5.py
@@ -5,3 +5,11 @@ my_weight = 180
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = "Brown"
+
+print "Lets talk about %s" %my_name
+print "He's %d inches tall." %my_height
+print "He's %d pounds heavy." %my_weight
+print "He's got %s eyes and %s hair." %(my_eyes,my_hair)
+print "His teeth are usually %s depending on the coffee." %my_teeth
+
+
