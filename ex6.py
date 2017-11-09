f231e1fb31243e5bc1da2502b746c553e78d998c
kevin <kevinochieng548@gmail.com>
2017-11-09 23:16:19 +0300

    print next variables

--- ex6.py
+++ ex6.py
@@ -7,3 +7,13 @@ print y
 
 print "I said: %s." %x
 print "I also said: %s." %y
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print joke_evaluation %hilarious
+
+w ="this is left side of ..."
+e = "a string with a right side"
+
+print w + e
