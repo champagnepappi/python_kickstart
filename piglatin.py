e6e3789c944f45f7f6edcdac4bf20f02d7bf5757
kevin <kevinochieng548@gmail.com>
2017-11-15 14:27:34 +0300

    slice the new word

--- piglatin.py
+++ piglatin.py
@@ -8,6 +8,7 @@ if len(original) > 0 and original.isalpha():
     word = original.lower()
     first = word[0]
     new_word = word + first + pyg
+    new_word = new_word[1:len(new_word)]
     print original
 else:
     print "empty"
