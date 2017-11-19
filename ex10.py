0a7cecf2daf99212ac8f53fb043bc2277805cdd7
kevin <kevinochieng548@gmail.com>
2017-11-19 22:06:34 +0300

    define multiline variable

--- ex10.py
+++ ex10.py
@@ -1,3 +1,10 @@
 tabby_cat = "\t I'm tabbed in."
 persian_cat = "I'm splt\non a line"
 backlash_cat = "I'm \\ a \\ cat"
+
+fat_cat = """
+I'll do a list
+\t* Cat food
+\t* fishies
+\t* Catnip\n\t* Grass
+"""
