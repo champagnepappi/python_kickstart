455cd620da31ec00318dca467341c144b23e9f87
kevin <kevinochieng548@gmail.com>
2017-12-01 10:38:19 +0300

    define students list

--- ex13.py
+++ ex13.py
@@ -1,2 +1,6 @@
-kevin = { "name": "Kevin", "homework": [],"tests": [] }
+kevin = { "name": "Kevin", "homework": ["css", "js"],"tests": [] }
+shaz = {"name": "sharon", "homework": ["react", "pyhton"], "tests": "lisp"}
 santos = { "languages" : ["ruby", "python", "java"], "level": "junior dev" }
+
+students = [kevin, shaz]
+
