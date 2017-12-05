b18eec90e37f2f0e8d9d506cf43ea0173d0cb4fb
kevin <kevinochieng548@gmail.com>
2017-12-05 23:03:55 +0300

    store raw input in variable likes

--- ex14.py
+++ ex14.py
@@ -7,3 +7,5 @@ prompt = '>'
 print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a few questions."
 print "Do you like me %s?" % user_name
+
+likes = raw_input(prompt)
