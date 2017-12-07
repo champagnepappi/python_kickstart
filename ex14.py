257d0eb4612ddab74436040cd6793acdcbd1bd78
kevin <kevinochieng548@gmail.com>
2017-12-07 22:57:04 +0300

    print out user input

--- ex14.py
+++ ex14.py
@@ -12,3 +12,13 @@ likes = raw_input(prompt)
 
 print "where do you live %s? " % user_name
 lives = raw_input(prompt)
+
+print "What kind of computer do you have?"
+computer = raw_input(prompt)
+
+print """
+ Alright so you said %r about liking me.
+ You live in %r. Not sure where that is.
+ And you have a %r computer. Nice.
+""" %(likes,lives,computer)
+
