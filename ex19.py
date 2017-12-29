8ea4e67083d201d08984ee43c9aa6c0216ac61c1
kevin <kevinochieng548@gmail.com>
2017-12-29 16:41:14 +0300

    print and call the defined method

--- ex19.py
+++ ex19.py
@@ -3,3 +3,6 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print "You have %d boxes of crackers!" % boxes_of_crackers
     print "Man that's enough for the party"
     print "Get a blanket. \n"
+
+print "We can just give the function numbers directly:"
+cheese_and_crackers(20,30)
