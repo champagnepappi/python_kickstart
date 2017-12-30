b4584377e403db3b72d7f035ed615147e5bc702f
kevin <kevinochieng548@gmail.com>
2017-12-30 22:18:04 +0300

    initialize variables

--- ex19.py
+++ ex19.py
@@ -6,3 +6,7 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
 
 print "We can just give the function numbers directly:"
 cheese_and_crackers(20,30)
+
+print "OR, we can use variables from our script "
+amount_of_cheese = 10
+amount_of_crackers = 50
