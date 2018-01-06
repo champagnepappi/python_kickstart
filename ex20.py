09e7e0c38329f42dc27246465e5e7a3c283f2943
kevin <kevinochieng548@gmail.com>
2018-01-06 21:58:25 +0300

    print third line

--- ex20.py
+++ ex20.py
@@ -28,3 +28,6 @@ print_a_line(current_line, current_file)
 
 current_line = current_line + 1
 print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
