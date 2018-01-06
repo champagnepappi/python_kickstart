74dcfadf6a6eae7dde722c977a4575e06e5f3b0a
kevin <kevinochieng548@gmail.com>
2018-01-06 21:57:28 +0300

    print second line

--- ex20.py
+++ ex20.py
@@ -24,5 +24,7 @@ rewind(current_file)
 print "Let's print three lines"
 
 current_line = 1
+print_a_line(current_line, current_file)
 
+current_line = current_line + 1
 print_a_line(current_line, current_file)
