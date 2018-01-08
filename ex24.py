8d70d19edec209082ad365e248c76b58fe9d5fe1
kevin <kevinochieng548@gmail.com>
2018-01-08 12:48:29 +0300

    define the secret formula method

--- ex24.py
+++ ex24.py
@@ -14,3 +14,14 @@ print "------------------------"
 print poem
 print "------------------------"
 
+five = 10 - 2 + 3 - 6
+print "This should be five: %s"  % five
+
+def secret_formula(started):
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    crates = jars / 100
+    return jell_beans, jars, crates
+
+
+
