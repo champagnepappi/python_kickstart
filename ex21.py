f6372dc1a639bbe57563eb8eee75e28e894022ab
kevin <kevinochieng548@gmail.com>
2018-01-08 11:07:37 +0300

    initialize age,height, weight and iq variables

--- ex21.py
+++ ex21.py
@@ -10,7 +10,14 @@ def multiply(a,b):
     print "MULTIPLYING %d * %d" %(a,b)
     return a * b
 
-def divide(a,b)
+def divide(a,b):
     print "DIVIDING %d / %d" % (a,b)
     return a / b
+
+print "let's do some math with just functions!"
+
+age = add(30,5)
+height = subtract(87,3)
+weight = multiply(95,3)
+iq = divide(100,2)
 
