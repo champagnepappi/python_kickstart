9d9b92bfb5eae8c387baa242330c278cf1213374
kevin <kevinochieng548@gmail.com>
2018-01-09 12:46:31 +0300

    define print first and last method

--- ex25.py
+++ ex25.py
@@ -21,3 +21,9 @@ def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words"""
     words = break_words(sentence)
     return sort_words(words)
+
+def print_first_and_last(sentence):
+    """Print the first and last words of the sentence"""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
