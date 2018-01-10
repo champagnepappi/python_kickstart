13bbc94bdf5ae1551eec46d9e48548f81e14aedd
kevin <kevinochieng548@gmail.com>
2018-01-10 15:23:10 +0300

    define Person class

--- ex42.py
+++ ex42.py
@@ -8,3 +8,8 @@ class Dog(Animal):
 class Cat(Animal):
     def __init__(self, name):
         self.name = name
+
+class Person(object):
+    def __init__(self, name):
+        self.name = name
+        self.pet  = None
