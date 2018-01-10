d081671a30fdecae88d6c04253118e340b55d94d
kevin <kevinochieng548@gmail.com>
2018-01-10 11:07:55 +0300

    get input from user

---
+++ ex31.py
@@ -0,0 +1,11 @@
+print "You enter a dark room with two doors. Do you go through #door! or #door2"
+
+door = raw_input(">")
+
+if door == "1":
+    print "There's a giant bear here eating a cheese cake. What do you do?"
+    print "1. Take the Cake"
+    print "2. Scream at the bear"
+
+    bear = raw_input(">")
+
