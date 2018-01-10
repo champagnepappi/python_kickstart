5b03bc162852a23d1c7e9fd867500acb4307fe2a
kevin <kevinochieng548@gmail.com>
2018-01-10 14:05:45 +0300

    use join

--- ex38.py
+++ ex38.py
@@ -10,3 +10,13 @@ while len(stuff) != 10:
     print "Adding: ", next_one
     stuff.append(next_one)
     print "There's %d items now." %len(stuff)
+
+print "There we go: ", stuff
+
+print "Let's do some things with stuff"
+
+print stuff[1]
+print stuff[-1]
+print stuff.pop()
+print ' '.join(stuff)
+print '#'.join(stuff[3:5])
