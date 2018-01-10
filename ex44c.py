618d3e37b741dfc6bd8cb1d9edb96d5fa6757881
kevin <kevinochieng548@gmail.com>
2018-01-10 15:58:41 +0300

    override the altered method

--- ex44c.py
+++ ex44c.py
@@ -13,6 +13,11 @@ class Child(Parent):
     def override(self):
         print "CHILD override()"
 
+    def altered(self):
+        print "CHILD, BEFORE PARENT altered"
+        super(Child, self).altered()
+        print "CHILD, AFTER PARENT altered"
+
 dad = Parent()
 son = Child()
 
