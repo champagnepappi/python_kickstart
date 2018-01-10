1e31551621de2262bd9bf99736d2d14951faca00
kevin <kevinochieng548@gmail.com>
2018-01-10 11:16:33 +0300

    print results of option 2

--- ex31.py
+++ ex31.py
@@ -15,3 +15,9 @@ if door == "1":
         print "The bear eats your legs off. Good job!"
     else:
         print "Well, doing %s is probably better. Bear runs away" %bear
+
+elif door == "2":
+    print "You stare into the endless abyss at Cthulhu's retina"
+    print "1. Blueberries"
+    print "2.  Yellow jacket clothespins"
+    print "3. Understanding revolvers yelling melodies"
