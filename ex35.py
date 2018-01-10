70a88e0ecafa105ccfc989fbd88df9753fad4419
kevin <kevinochieng548@gmail.com>
2018-01-10 13:09:24 +0300

    use the exit function

--- ex35.py
+++ ex35.py
@@ -8,3 +8,9 @@ def gold_room():
         how_much = int(next)
     else:
         dead("Man, learn to type a number")
+
+    if how_much < 50:
+        print "Nice, you're not greedy, you win"
+        exit(0)
+    else:
+        dead("You greedy bastard")
