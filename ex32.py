de00a6bb2a8d4424255736a654ca694d6d5e7997
kevin <kevinochieng548@gmail.com>
2018-01-10 11:31:26 +0300

    loop through the count list

--- ex32.py
+++ ex32.py
@@ -1,3 +1,7 @@
 the_count = [1,2,3,4,5,1231,21,12]
 fruits = ['apples', 'oranges','pears','apricots']
 change = [1,'pennies', 32, 'dimes',5,'guwa']
+
+for count in the_count:
+    print "Count is: %d" % count
+
