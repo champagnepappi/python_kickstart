da3b9fb5338bb4fc1543643b08b4a9f7a59645c0
kevin <kevinochieng548@gmail.com>
2018-01-10 11:12:37 +0300

    use if statement to check for available options

--- ex31.py
+++ ex31.py
@@ -9,3 +9,9 @@ if door == "1":
 
     bear = raw_input(">")
 
+    if bear == "1":
+        print "The bear eats your face. Good job"
+    elif bear == "2":
+        print "The bear eats your legs off. Good job!"
+    else:
+        print "Well, doing %s is probably better. Bear runs away" %bear
